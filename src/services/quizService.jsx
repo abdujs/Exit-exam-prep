80f99fa22b2bbf0a326335e1209c32d9f10ec947
abdujs <abdu12.aau@gmail.com>
2025-04-01 13:49:18 +0300

    refactor: remove quiz-related functions from quizService as they are no longer needed

--- src/services/quizService.jsx
+++ src/services/quizService.jsx
@@ -1,17 +1,4 @@
 import { db } from '../config/firebaseConfig';
 import { collection, doc, addDoc, getDocs } from 'firebase/firestore';
 
-const quizzesCollection = collection(db, "quizzes");
-
-// Add a new quiz
-export const addQuiz = async (courseId, title, questions) => {
-  await addDoc(quizzesCollection, { courseId, title, questions });
-};
-
-// Fetch quizzes by course
-export const getQuizzesByCourse = async (courseId) => {
-  const quizzesSnapshot = await getDocs(quizzesCollection);
-  return quizzesSnapshot.docs
-    .map(doc => ({ id: doc.id, ...doc.data() }))
-    .filter(quiz => quiz.courseId === courseId);
-};
+// No quiz-related functions remain in this file.
